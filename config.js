7bf7a61ca15bf1b8f2218dbb50ae23f56013f9f4
zaheernew <zaheernew@gmail.com>
2025-07-18 11:45:40 +0530

    Initial commit

---
+++ config.js
@@ -0,0 +1,9 @@
+var settings = {
+    downloadTestDuration: 15,   // seconds
+    uploadTestDuration: 15,     // seconds
+    maxDownloadSize: 20,        // MB
+    maxUploadSize: 20,          // MB
+    pingCount: 10,
+    telemetry_enabled: false,
+    test_order: "D_U_P_J"       // Download, Upload, Ping, Jitter
+};
